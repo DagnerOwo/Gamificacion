1b74d98d1fc1265c87c47151bad2175efafbff92
DagnerOwo <dagnerronaldoramossacari@gmail.com>
2021-04-23 12:42:53 +0200

    Blank project

---
+++ src/features/account/components/account.styles.js
@@ -0,0 +1,7 @@
+import styles from 'styled-components';
+import React from 'react';
+import { TextInput } from 'react-native-paper';
+
+export const UserInput = styled(TextInput)`
+width: 300px
+`;
---
+++ src/features/account/screens/login.screen.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { View } from 'react-native'
+import { UserInput } from '../components/account.styles';
+export const LoginScreen = () => {
+
+    return (
+        <View>
+            <UserInput></UserInput>
+        </View>
+    )
+}
